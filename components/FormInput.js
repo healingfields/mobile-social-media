51c53b9590476838dbf0a0996fc81cb348559159
healingfields <dihratene@gmail.com>
2022-07-31 13:06:36 +0100

    created FormInput component

---
+++ components/FormInput.js
@@ -0,0 +1,26 @@
+import {StyleSheet, TextInput} from "react-native";
+
+const styles = StyleSheet.create({
+    input:{
+        width:'100%',
+        padding:20,
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"space-around",
+        borderRadius:5,
+        borderWidth:1,
+        borderColor:"black",
+        backgroundColor:"white",
+        fontSize:20,
+        marginBottom:20,
+    }
+})
+export default function FormInput({onChange, ...props}){
+    return(
+    <TextInput
+        style={styles.input}
+        onChangeText={(text)=>onChange(text)}
+        {...props}
+        />
+    );
+}
