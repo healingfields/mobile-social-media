1bf854c634e9cada244ab6a12c83a75ba7b2a716
healingfields <dihratene@gmail.com>
2022-08-01 12:57:26 +0100

    created the posts screen

---
+++ screens/Posts.js
@@ -0,0 +1,44 @@
+import {ScrollView, StyleSheet, Text, TouchableOpacity} from "react-native";
+import {useContext, useEffect} from "react";
+import {PostsContext} from "../context/PostsContext";
+import PostItem from "../components/PostItem";
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'white'
+    },
+    message:{
+        color:'red',
+        fontSize:20
+    },
+
+})
+export default function Posts(){
+
+    // const navigation = useNavigation();
+
+    const {loading, error, posts, fetchPosts} = useContext(PostsContext);
+
+    useEffect(()=>{
+        (!posts || !posts.length) && fetchPosts();
+        console.log(loading);
+    }, [posts, fetchPosts])
+
+    return(
+        <ScrollView style={styles.container}>
+            {loading || error ? (
+                <Text style={styles.message}>{error || 'Loading...'}</Text>
+            ):(
+                posts &&
+                    posts.map((post)=>(
+                        <TouchableOpacity
+                            key={post.id}
+                        >
+                                <PostItem key={post.id} data={post}/>
+                        </TouchableOpacity>
+                    ))
+            )}
+        </ScrollView>
+    )
+}
